506f84ea787ac9b77cdd6c9d0797eff15a64ae62
dalinthat123-byte <dalinthat123@gmail.com>
2026-03-13 07:37:54 +0700

    First Commit

---
+++ bankacc1.py
@@ -0,0 +1,92 @@
+class BankAccount:
+    def __init__(self, name, balance, secret):
+        self.name = name
+        self.__balance = balance
+        self.__secret = secret
+
+    def withdraw(self):
+        print("=== Withdrawing Balance ===")
+        secret = input("Input your password:  ")
+
+        if secret == self.__secret:
+            amount = float(input("Input your amount:  "))
+            remain = self.__balance - amount
+
+            if remain < 0:
+                print("your balance is not enough to withdraw")
+            else:
+                self.__balance = remain
+                print("...Withdrawal Successfully...")
+                print(f"Your remaining balance is: {self.__balance}")
+        else:
+            print("sorry, incorrect password. Please, Try again.")
+
+    def Check_Balance(self):
+        print("=== Checking Balance ===")
+        secret = input("Input your password: ")
+
+        if secret == self.__secret:
+            print(f"Hello User: {self.name}")
+            print(f"Your remaining balance is {self.__balance}")
+        else:
+            print("sorry, incorrect password. Please, Try again.")
+
+    def deposit(self):
+        print("User is depositing")
+
+class SavingAccount(BankAccount):
+    def Calculate_Interest(self):
+        print(f"Your Balance now is {self._BankAccount__balance}")
+
+class StudentBankAccount(BankAccount):
+    def withdraw(self):
+        print("==== Student Withdraw ====")
+        secret = input("Input your password: ")
+
+        if secret == self._BankAccount__secret:
+            amount = float(input("Input your amount: "))
+
+            if amount > 500:
+                print("You can't withdraw more than 500$ per day")
+            else:
+                self._BankAccount__balance -= amount
+                print(f"Your current balance is {self._BankAccount__balance}")
+        else:
+            print("sorry, incorrect password. Please, Try again.")
+
+class PremiumSaving(SavingAccount):
+    def deposit(self):
+        print("==== Premium Deposit ====")
+        amount = float(input("Input deposit amount: "))
+
+        bonus = amount * 0.02
+        self._BankAccount__balance += amount + bonus
+
+        print(f"Bonus: {bonus}")
+        print(f"Your current balance is {self._BankAccount__balance}")
+ 
+class BusinessAccount(BankAccount):
+    def take_loan(self):
+        loan = float(input("Input loan amount: "))
+        self._BankAccount__balance += loan
+        print(f"your current balance is {self._BankAccount__balance}")
+
+rose = SavingAccount(name="rose", balance=30000, secret="1234")
+rose.withdraw()
+rose.Check_Balance()
+rose.Calculate_Interest()
+
+   
+student = StudentBankAccount(name="rose", balance=90000, secret="1234")
+student.withdraw()          
+student.Check_Balance()
+
+   
+premium = PremiumSaving(name="rose", balance=249000, secret="1234")
+premium.deposit()          
+premium.Calculate_Interest()
+
+    
+business = BusinessAccount(name="rose", balance=143000, secret="1234")
+business.take_loan()        
+business.Check_Balance()
